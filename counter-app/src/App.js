bd862c9369c91ce5179caf7c468d499ce4bec8eb
Katy Donoghue <kathrynjdonoghue@gmail.com>
2020-11-10 09:56:57 -0500

    finished counter app

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
+import DisplayCounter from "./components/DisplayCounter"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      counter: 0
+    }
+
+  }
+
+  handleIncrementClick = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <DisplayCounter ctr = {this.state.counter}/>
+        <button onClick={this.handleIncrementClick}>Increment</button>
+
+      </div>
+    )
+  }
+
 }
 
 export default App;
---
+++ counter-app/src/components/DisplayCounter.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class DisplayCounter extends Component {
+
+        render() {
+        return <h1>{this.props.ctr}</h1>
+        }
+}
+
+
+
+export default DisplayCounter

75fb76ff48a10f9c4c09fefbb5f858438d80a613
Katy Donoghue <kathrynjdonoghue@gmail.com>
2020-11-12 11:47:54 -0500

    added counter written with hooks

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,46 +1,72 @@
-import React, {Component} from 'react'
-// import DisplayCounter from "./components/DisplayCounter"
-import './App.css';
-// import Stepper from "./components/Stepper"
-import News from "./components/News"
+// import React, {Component} from 'react'
+// // import DisplayCounter from "./components/DisplayCounter"
+// import './App.css';
+// // import Stepper from "./components/Stepper"
+// import News from "./components/News"
 
 
-class App extends Component {
+// class App extends Component {
 
-  constructor() {
-    super()
-    this.state = {
-      news: []
-    }
+//   constructor() {
+//     super()
+//     this.state = {
+//       news: []
+//     }
+
+//   }
+
+//   fetchNews = () => {
+//     fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=0cf790498275413a9247f8b94b3843fd")
+//     .then(response => response.json())
+//     .then(result => {
+//       console.log(result)
+//       this.setState({
+//         news: result.articles
+//       })
+//     })
+//   }
+
+//   componentDidMount() {
+//     this.fetchNews()
+//   }
+
+
+//   render () {
+//     return (
+//       <div>
+//       {/* <DisplayCounter />
+//       <Stepper /> */}
+//       <News news={this.state.news} />
+//       </div>
+//     )
+//   }
+
+// }
+
+/********************* Above is Classes, below is Hooks ************************** */ 
+
+import React, {useState} from 'react'
+
+function App() {
+
+  const[count, setCount] = useState(0)
+
+  const handleDecrement = () => {
+    setCount(count - 1)
 
   }
 
-  fetchNews = () => {
-    fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=0cf790498275413a9247f8b94b3843fd")
-    .then(response => response.json())
-    .then(result => {
-      console.log(result)
-      this.setState({
-        news: result.articles
-      })
-    })
+  const handleIncrement = () => {
+    setCount(count + 1)
   }
 
-  componentDidMount() {
-    this.fetchNews()
-  }
-
-
-  render () {
-    return (
-      <div>
-      {/* <DisplayCounter />
-      <Stepper /> */}
-      <News news={this.state.news} />
-      </div>
-    )
-  }
-
+  return (
+    <div>
+      <button onClick={handleDecrement}>-</button>
+      <h1>{count}</h1>
+      <button onClick={handleIncrement}>+</button>
+    </div>
+  )
 }
 
 export default App;
